c546d65cefc4dcdf862638fdec67aae9911cb4f0
RazerEdge64 <kohirkarshubhang@gmail.com>
2025-03-24 19:13:03 -0400

    llm summarizer check

--- test.js
+++ test.js
@@ -1,5 +1,34 @@
 console.log('Hi there, CodeSieve!');
 
+// Old logging
 console.log("Checking diff-pr-pipeline");
-
 console.log("Checking new PR difference with the parser");
+
+// Dummy function
+function sum(a, b) {
+  return a + b;
+}
+
+// Unused code
+function unusedHelper() {
+  console.log("This is never called");
+}
+
+// Add a new feature
+function greetUser(name) {
+  if (!name) {
+    console.log("Hello, guest!");
+  } else {
+    console.log("Hello, " + name);
+  }
+}
+
+// Simulate a bug
+function buggyLogic(x) {
+  if (x = 5) { // <-- Assignment instead of comparison
+    console.log("X is 5");
+  }
+}
+
+greetUser("Shubhang");
+buggyLogic(4);
